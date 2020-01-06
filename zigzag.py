ebca5fe8777515891ceeb4a5748097ea4c300d43
Abe M <kinofevil@gmail.com>
2020-01-06 21:33:56 +0000

    pretends to work

--- zigzag.py
+++ zigzag.py
@@ -14,25 +14,27 @@ class zigzag():
         if not zigzag.matrix:
             for level in range(numlevels):
                 zigzag.matrix[level] = ['-' for i in range(len(string))]
-        while (zigzag.counter <= len(string)) for level in range(numlevels):
-            line = zigzag.matrix[level]
-            line[zigzag.travelator] = string[zigzag.counter]
-            zigzag.counter += 1
-        else:
-            return f'Done zig, {zigzag.matrix}'
+        for level in range(numlevels):
+            if (zigzag.counter < len(string)):
+                line = zigzag.matrix[level]
+                line[zigzag.travelator] = string[zigzag.counter]
+                zigzag.counter += 1
+            else:
+                return f'Done zig, {zigzag.matrix}'
         # print(zigzag.matrix)
         self.zag(string, numlevels)
         
 
     def zag(self, string, numlevels):
         levelcorrection = 2
-        while (zigzag.counter <= len(string)) for level in range(numlevels - levelcorrection, -1, -1):
-            line = zigzag.matrix[level]
-            zigzag.travelator += 1
-            line[zigzag.travelator] = string[zigzag.counter]
-            zigzag.counter += 1
-        else:
-            return f'Done zag, {zigzag.matrix}'
+        for level in range(numlevels - levelcorrection, -1, -1):
+            if (zigzag.counter < len(string)):
+                line = zigzag.matrix[level]
+                zigzag.travelator += 1
+                line[zigzag.travelator] = string[zigzag.counter]
+                zigzag.counter += 1
+            else:
+                return f'Done zag, {zigzag.matrix}'
         zigzag.travelator += 1
         # print(zigzag.matrix)
         self.zig(string, numlevels)
@@ -42,7 +44,7 @@ class zigzag():
         totalchars = ''
         self.zig(string, numlevels)
         for char in zigzag.matrix:
-            if char != '-':
+            if char != '-' and isinstance(char, str):
                 totalchars += char
             else:
                 continue
