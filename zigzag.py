ba4526dc9759c2d7125de3324a854a008133645e
abeisherald <kinofevil@gmail.com>
2020-01-05 21:33:35 -0500

    why no worky

---
+++ zigzag.py
@@ -0,0 +1,43 @@
+string = 'paypalishungry'
+numlevels = 3
+
+
+class zigzag():
+    def __init__(self):
+        pass
+
+    matrix = {}
+    counter = 0
+    travelator = 0
+
+    def zig(self, string, numlevels):
+        while zigzag.counter < len(string):
+            if not zigzag.matrix:
+                for level in range(numlevels):
+                    zigzag.matrix[level] = [
+                        '-' for i in range(len(string))]
+            for level in range(numlevels):
+                line = zigzag.matrix[level]
+                line[zigzag.travelator] = string[zigzag.counter]
+                zigzag.counter += 1
+            # print(zigzag.matrix)
+            self.zag(string, numlevels)
+        else:
+            return f'Done zig, {zigzag.matrix}'
+
+    def zag(self, string, numlevels):
+        while zigzag.counter < len(string):
+            levelcorrection = 2
+            for level in range(numlevels - levelcorrection, -1, -1):
+                line = zigzag.matrix[level]
+                zigzag.travelator += 1
+                line[zigzag.travelator] = string[zigzag.counter]
+                zigzag.counter += 1
+            zigzag.travelator += 1
+            # print(zigzag.matrix)
+            self.zig(string, numlevels)
+        else:
+            return f'Done zag, {zigzag.matrix}'
+
+
+zigzag().zig(string, numlevels)
