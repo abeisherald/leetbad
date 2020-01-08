d40d6a6b57aaebea34ace885e93a1abbc288b6d1
abeisherald <kinofevil@gmail.com>
2020-01-08 18:44:04 -0500

    to be

--- zigzag.py
+++ zigzag.py
@@ -9,41 +9,28 @@ class zigzag():
     matrix = {}
     counter = 0
     travelator = 0
-    levelstart = 0
 
     def zig(self, string, numlevels):
-        if not zigzag.matrix:
-            for level in range(numlevels):
-                zigzag.matrix[level] = ['-' for i in range(len(string))]
-        for level in range(zigzag.levelstart, numlevels):
-            if (zigzag.counter < len(string)):
-                line = zigzag.matrix[level]
-                line[zigzag.travelator] = string[zigzag.counter]
-                zigzag.counter += 1
-            else:
-                return f'Done zig, {zigzag.matrix}'
-        # print(zigzag.matrix)
-        self.zag(string, numlevels)
-        
+        for level in range(numlevels):
+            line = zigzag.matrix[level]
+            line[zigzag.travelator] = string[zigzag.counter]
+            zigzag.counter += 1
 
     def zag(self, string, numlevels):
         levelcorrection = 2
-        for level in range(numlevels - levelcorrection, -1, -1):
-            if (zigzag.counter < len(string)):
-                line = zigzag.matrix[level]
-                zigzag.travelator += 1
-                line[zigzag.travelator] = string[zigzag.counter]
-                zigzag.counter += 1
-            else:
-                return f'Done zag, {zigzag.matrix}'
-        zigzag.levelstart = 1
-        # print(zigzag.matrix)
-        self.zig(string, numlevels)
-        
+        for level in range(numlevels - levelcorrection, 0, -1):
+            line = zigzag.matrix[level]
+            zigzag.travelator += 1
+            line[zigzag.travelator] = string[zigzag.counter]
+            zigzag.counter += 1
 
     def zoog(self, string, numlevels):
         totalchars = ''
-        self.zig(string, numlevels)
+        for level in range(numlevels):
+            zigzag.matrix[level] = '-' * len(string)
+        while zigzag.counter < len(string):
+            self.zig(string, numlevels)
+            self.zag(string, numlevels)
         listoflines = list(zigzag.matrix.values())
         for line in listoflines:
             for char in line:
