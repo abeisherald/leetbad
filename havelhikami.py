d7d88b52b1314fc1ddd1353220b350b2fc488514
abeisherald <kinofevil@gmail.com>
2020-01-08 20:43:01 -0500

    the fuck

--- havelhikami.py
+++ havelhikami.py
@@ -1,26 +1,29 @@
-lies = [5,3,0,2,6,2,0,7,2,5]
-frontelimination = 2
+lies = [5, 3, 0, 2, 6, 2, 0, 7, 2, 5]
+
 
 def detector(statement):
-    statement = sorted(statement, reverse=True)
+    holdanswer = ''
+    statement = sorted(statement)
     for answer in statement:
         if answer == 0:
             statement.remove(answer)
     if statement is False:
         return True
-    statement = statement.remove(answer)
     for answer in statement:
-        if answer > len(statement):
+        holdanswer = statement.pop(statement.index(answer))
+        break
+    for answer in statement:
+        if holdanswer > len(statement):
             return False
         else:
-            return True
+            break
     statement = sorted(statement, reverse=True)
-    for answer in statement[:frontelimination]:
-        answeridx = statement.index(answer)
+    for answer in statement[:holdanswer]:
         answer -= 1
-        statement[answeridx] = answer
-    
+
     statement = sorted(statement, reverse=True)
     print(statement)
+    detector(statement)
 
-detector(lies)
+
+print(detector(lies))
