4ed6514fecfdb3f19b72ae98cb3eee96e82b9547
abeisherald <kinofevil@gmail.com>
2020-01-08 21:32:29 -0500

    done

--- havelhikami.py
+++ havelhikami.py
@@ -3,26 +3,16 @@ lies = [5, 3, 0, 2, 6, 2, 0, 7, 2, 5]
 
 def detector(statement):
     holdanswer = ''
-    statement = sorted(statement)
-    for answer in statement:
-        if answer == 0:
-            statement.remove(answer)
+    while 0 in statement:
+        statement.remove(0)
     if statement is False:
         return True
-    for answer in statement:
-        holdanswer = statement.pop(statement.index(answer))
-        break
-    for answer in statement:
-        if holdanswer > len(statement):
-            return False
-        else:
-            break
     statement = sorted(statement, reverse=True)
-    for answer in statement[:holdanswer]:
-        answer -= 1
-
-    statement = sorted(statement, reverse=True)
-    print(statement)
+    holdanswer = statement.pop(0)
+    if holdanswer > len(statement):
+        return False
+    for answer_idx in range(holdanswer):
+        statement[answer_idx] -= 1
     detector(statement)
 
 
